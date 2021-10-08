67c615b51a03e83a983b3c3efa7c6807fc4019f6
Leonardo Morales <leonardo_moralesespinosa@live.com.mx>
2021-10-07 19:21:42 -0500

    Animaciones de Transicion

--- app/src/main/java/dev/leonardom/androidintroduccion/presentation/product_detail/ProductDetailFragment.kt
+++ app/src/main/java/dev/leonardom/androidintroduccion/presentation/product_detail/ProductDetailFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import dev.leonardom.androidintroduccion.databinding.FragmentProductDetailBinding
 
 class ProductDetailFragment : Fragment() {
@@ -25,7 +26,8 @@ class ProductDetailFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.btnPurchase.setOnClickListener {
-
+            val action = ProductDetailFragmentDirections.actionProductDetailFragmentToShoppingCartFragment()
+            findNavController().navigate(action)
         }
     }
 
