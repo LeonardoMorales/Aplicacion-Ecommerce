6cc803b9437890c9d03cf04f5460816561e61d95
Leonardo Morales <leonardo_moralesespinosa@live.com.mx>
2021-10-07 20:01:17 -0500

    Mostrar Detalles de Producto

--- app/src/main/java/dev/leonardom/androidintroduccion/presentation/product_detail/ProductDetailFragment.kt
+++ app/src/main/java/dev/leonardom/androidintroduccion/presentation/product_detail/ProductDetailFragment.kt
@@ -6,12 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import coil.load
 import dev.leonardom.androidintroduccion.databinding.FragmentProductDetailBinding
 
 class ProductDetailFragment : Fragment() {
 
     private var _binding: FragmentProductDetailBinding? = null
     private val binding get() = _binding!!
+
+    private val args: ProductDetailFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,6 +28,11 @@ class ProductDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.imageViewProduct.load(args.product.imageURL)
+        binding.textViewProductName.text = args.product.name
+        binding.textViewProductDescription.text = args.product.description
+        binding.textViewProductPrice.text = "$ ${args.product.price} MXN"
 
         binding.btnPurchase.setOnClickListener {
             val action = ProductDetailFragmentDirections.actionProductDetailFragmentToShoppingCartFragment()
