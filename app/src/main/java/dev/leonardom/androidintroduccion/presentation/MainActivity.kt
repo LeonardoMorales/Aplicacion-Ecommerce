cbd6c8484e3d1c043cdfeb15c5a5e1469cbbbfb2
Leonardo Morales <leonardo_moralesespinosa@live.com.mx>
2021-10-11 17:08:33 -0500

    Navegacion responsiva en distintas IU

--- app/src/main/java/dev/leonardom/androidintroduccion/presentation/MainActivity.kt
+++ app/src/main/java/dev/leonardom/androidintroduccion/presentation/MainActivity.kt
@@ -1,11 +1,12 @@
 package dev.leonardom.androidintroduccion.presentation
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.*
 import dev.leonardom.androidintroduccion.R
 import dev.leonardom.androidintroduccion.databinding.ActivityMainBinding
 
@@ -14,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,8 +25,68 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         navController = navHostFragment.findNavController()
 
+        appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = setOf(
+                R.id.productListFragment,
+                R.id.favoritesFragment,
+                R.id.shoppingCartFragment
+            ),
+            drawerLayout = binding.drawerLayout
+        )
 
-        binding.bottomNavigationView.setupWithNavController(navController)
+        // BottomNavigationView
+        binding.bottomNavigationView?.setupWithNavController(navController)
 
+        // Toolbar
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        // NavigationRailView
+        binding.navigationRailView?.setOnItemSelectedListener { menuItem ->
+            when(menuItem.itemId) {
+                R.id.productListFragment -> {
+                    navController.navigate(R.id.productListFragment)
+                    true
+                }
+                R.id.favoritesFragment -> {
+                    navController.navigate(R.id.favoritesFragment)
+                    true
+                }
+                R.id.shoppingCartFragment -> {
+                    navController.navigate(R.id.shoppingCartFragment)
+                    true
+                }
+                else -> false
+            }
+        }
+
+        // DrawerLayout
+        binding.navView?.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
